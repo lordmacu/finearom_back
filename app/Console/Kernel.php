fa5f41851eabf5720f21a5967b5b61df076cbf39
cristiangarcia-source <cristiangarcia-source@users.noreply.github.com>
2026-02-16 13:45:17 -0500

    descomentar emails dispatch

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -46,7 +46,7 @@ class Kernel extends ConsoleKernel
             });
 
         // Ejecutar dispatch de emails de cartera cada 30 minutos
-   // $schedule->command('emails:dispatch')->everyMinute();
+            $schedule->command('emails:dispatch')->everyMinute();
 
    
 
